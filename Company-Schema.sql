a8d3713f0f8d004111283629b44013803fb7225a
Tinku Bansal <tinkubansal21@gmail.com>
2021-05-20 20:15:07 +1000

    Created seed file

---
+++ Company-Schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE database company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
+
