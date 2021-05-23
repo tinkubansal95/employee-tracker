e1253e10f5ffce576c1c00a3ac0715a231590e60
Tinku Bansal <tinkubansal21@gmail.com>
2021-05-23 17:24:19 +1000

    fixed code

--- server.js
+++ server.js
@@ -7,13 +7,10 @@ const cTable = require('console.table');
 const connection = mysql.createConnection({
   host: 'localhost',
 
-  // Your port; if not 3306
   port: 3306,
 
-  // Your username
   user: process.env.DB_USER,
 
-  // Be sure to update with your own MySQL password!
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
